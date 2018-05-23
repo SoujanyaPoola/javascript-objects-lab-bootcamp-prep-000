4fd36d5aed02d8c021c683c44b63198fcbfd67a8
Souji 2613 <puppypoola@gmail.com>
2018-05-23 15:34:58 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+
+  const recipes = {veg: 'milk'}
+  
+
+function updateObjectWithKeyAndValue(ob, key, value){
+  var newObj = Object.assign({}, ob);
+  newObj[key] = value ;
+  return newObj;
+  
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+   object[key] = value ;
+  return object;
+  
+}
+/*function deleteFromObjectByKey(object, key){
+  var newObj = Object.assign({},object);
+  delete newObj.key;
+  
+}*/
